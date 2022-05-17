58035cdb2cbfa45129bb33999f7e19b3adc158cf
mmg <marcel@lxm-studios.eu>
2022-05-17 20:38:29 +0200

    Remade litterally everything

--- PCleaner/Form1.Designer.cs
+++ PCleaner/Form1.Designer.cs
@@ -28,19 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.selectButton = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialDivider2 = new MaterialSkin.Controls.MaterialDivider();
+            this.customSelectButton = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel3 = new MaterialSkin.Controls.MaterialLabel();
+            this.stuffLabel = new MaterialSkin.Controls.MaterialLabel();
             this.SuspendLayout();
+            // 
+            // selectButton
+            // 
+            this.selectButton.AutoSize = true;
+            this.selectButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.selectButton.Depth = 0;
+            this.selectButton.Icon = null;
+            this.selectButton.Location = new System.Drawing.Point(75, 135);
+            this.selectButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Primary = true;
+            this.selectButton.Size = new System.Drawing.Size(69, 36);
+            this.selectButton.TabIndex = 0;
+            this.selectButton.Text = "Select";
+            this.selectButton.UseVisualStyleBackColor = true;
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(50, 97);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(127, 19);
+            this.materialLabel1.TabIndex = 1;
+            this.materialLabel1.Text = "Select a language";
+            this.materialLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // materialDivider2
+            // 
+            this.materialDivider2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialDivider2.Depth = 0;
+            this.materialDivider2.Location = new System.Drawing.Point(12, 187);
+            this.materialDivider2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialDivider2.Name = "materialDivider2";
+            this.materialDivider2.Size = new System.Drawing.Size(208, 10);
+            this.materialDivider2.TabIndex = 2;
+            this.materialDivider2.Text = "materialDivider1";
+            // 
+            // customSelectButton
+            // 
+            this.customSelectButton.AutoSize = true;
+            this.customSelectButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.customSelectButton.Depth = 0;
+            this.customSelectButton.Icon = null;
+            this.customSelectButton.Location = new System.Drawing.Point(75, 251);
+            this.customSelectButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.customSelectButton.Name = "customSelectButton";
+            this.customSelectButton.Primary = true;
+            this.customSelectButton.Size = new System.Drawing.Size(69, 36);
+            this.customSelectButton.TabIndex = 0;
+            this.customSelectButton.Text = "Select";
+            this.customSelectButton.UseVisualStyleBackColor = true;
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel2.Location = new System.Drawing.Point(21, 200);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(179, 19);
+            this.materialLabel2.TabIndex = 1;
+            this.materialLabel2.Text = "or a custom language file";
+            this.materialLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // materialLabel3
+            // 
+            this.materialLabel3.AutoSize = true;
+            this.materialLabel3.Depth = 0;
+            this.materialLabel3.Font = new System.Drawing.Font("Roboto", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.materialLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel3.Location = new System.Drawing.Point(12, 219);
+            this.materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel3.Name = "materialLabel3";
+            this.materialLabel3.Size = new System.Drawing.Size(198, 19);
+            this.materialLabel3.TabIndex = 1;
+            this.materialLabel3.Text = "language template on github";
+            this.materialLabel3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // stuffLabel
+            // 
+            this.stuffLabel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.stuffLabel.AutoSize = true;
+            this.stuffLabel.Depth = 0;
+            this.stuffLabel.Font = new System.Drawing.Font("Roboto", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.stuffLabel.ForeColor = System.Drawing.Color.Blue;
+            this.stuffLabel.Location = new System.Drawing.Point(2, 338);
+            this.stuffLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.stuffLabel.Name = "stuffLabel";
+            this.stuffLabel.Size = new System.Drawing.Size(55, 19);
+            this.stuffLabel.TabIndex = 3;
+            this.stuffLabel.Text = "GitHub";
+            this.stuffLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.stuffLabel.Click += new System.EventHandler(this.stuffLabel_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(232, 361);
+            this.Controls.Add(this.stuffLabel);
+            this.Controls.Add(this.materialDivider2);
+            this.Controls.Add(this.materialLabel3);
+            this.Controls.Add(this.materialLabel2);
+            this.Controls.Add(this.customSelectButton);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.selectButton);
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PCleaner - Language Select";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private MaterialSkin.Controls.MaterialRaisedButton selectButton;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialDivider materialDivider2;
+        private MaterialSkin.Controls.MaterialRaisedButton customSelectButton;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private MaterialSkin.Controls.MaterialLabel materialLabel3;
+        private MaterialSkin.Controls.MaterialLabel stuffLabel;
     }
 }
